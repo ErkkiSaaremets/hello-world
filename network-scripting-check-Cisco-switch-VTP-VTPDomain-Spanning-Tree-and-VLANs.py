5a495cf9dce95b6a399588343378a12c8718fc8f
Erkki Saaremets <erkki.saaremets@mail.ee>
2019-05-09 11:16:43 +0300

    check cisco 1

---
+++ network-scripting-check-Cisco-switch-VTP-VTPDomain-Spanning-Tree-and-VLANs.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python2.7
+# Was in SMN NOC and had to check if customers switches configuration is correct.
+# You can either be a masochist and do it manually one-by-one or
+# automate the checking hundreds of devices. 
+  
+
+import re,os
+path = "./network-stuff/"
+switches = os.listdir(path)
+
+def ragamoffin( check ):
+    if re.match("(.*)(Operating)(.*)", check):
+        print ("Current Switch VTP mode: "), check,
+    if re.match("(.*)(Domain)(.*)", check):
+        print ("Current Switch VTP domain: "), check,
+    if re.match("(.*)(rapid-pvst)(.*)", check):
+        print ("Spanning-Tree-Mode is rapid-pvst: "), check,
+
+    if re.match("(.*)([0123456789].*.active)(.*)", check):
+        print "VLANs ", check[0:14], "\n"
+    return 0
+
+for switch in switches:
+    file = os.path.join(path, switch)
+    text = open(file, "r")
+    print(file)
+    vlanwritten = False
+
+    for line in text:
+        ragamoffin(line)
+    print("")
